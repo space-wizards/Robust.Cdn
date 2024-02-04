cda59d444347fbf485935dee5902a5bdb62dafb4
Debug <49997488+DebugOk@users.noreply.github.com>
2024-02-04 19:34:22 +0100

    Add StatusController

---
+++ Robust.Cdn/Controllers/StatusController.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using Dapper;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Robust.Cdn.Controllers;
+
+[ApiController]
+public class StatusController : ControllerBase
+{
+    private readonly Database _db;
+
+    public StatusController(Database db)
+    {
+        _db = db;
+    }
+
+    [HttpGet("control/status")]
+    public async Task<IActionResult> GetControlStatus()
+    {
+        var con = _db.Connection;
+        con.BeginTransaction(deferred: true);
+
+        var versionCount = con.QuerySingleOrDefault<int>(
+            "SELECT COUNT(Id) FROM ContentVersion");
+
+        var assemblyVersion = Assembly.GetExecutingAssembly().GetName().Version;
+
+        return Ok(new
+        {
+            Status = "OK",
+            Version = assemblyVersion?.ToString() ?? "Unknown",
+            ContentVersions = versionCount
+        });
+    }
+}
