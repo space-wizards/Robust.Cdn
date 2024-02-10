a5ccc9af58d3d41b390378ec2a22718bacc21101
Debug <49997488+DebugOk@users.noreply.github.com>
2024-02-10 18:58:22 +0100

    Add a status endpoint (#3)
    * Add StatusController
    
    * Add some improvements

---
+++ Robust.Cdn/Controllers/StatusController.cs
@@ -0,0 +1,39 @@
+﻿using System.Reflection;
+using Dapper;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Robust.Cdn.Controllers;
+
+[ApiController]
+public class StatusController : ControllerBase
+{
+    private readonly Database _db;
+
+    public StatusController(Database db)
+    {
+        _db = db;
+    }
+
+    [HttpGet("control/status")]
+    public IActionResult GetControlStatus()
+    {
+        try
+        {
+            var con = _db.Connection;
+            var versionCount = con.QuerySingleOrDefault<int>("SELECT COUNT(Id) FROM ContentVersion");
+
+            var assemblyVersion = Assembly.GetExecutingAssembly().GetName().Version;
+
+            return Ok(new
+            {
+                Status = "OK",
+                Version = assemblyVersion?.ToString() ?? "Unknown",
+                ContentVersions = versionCount
+            });
+        }
+        catch
+        {
+            return StatusCode(500, new { Status = "Internal Server Error" });
+        }
+    }
+}
